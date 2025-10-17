03544115d2489ba2ef75506e5a3b9706a55920f1
warlleyfreitas <warlley.freitas@gmail.com>
2025-10-17 09:36:41 -0300

    Revert "Refatorando as specs and atualizando config de xpath do project"
    This reverts commit 14259d004317a25d613c4cead97bee77f7550cc9.

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
