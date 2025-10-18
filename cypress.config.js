ab8f72455ce8f582e8b66385f42ac67542659577
warlleyfreitas <warlley.freitas@gmail.com>
2025-10-18 10:23:37 -0300

    Atualizando cypress config com retry

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  retries: {
+    // Configure retry attempts for `cypress run`
+    runMode: 2,
+    // Configure retry attempts for `cypress open`
+    openMode: 0
+  },
   e2e: {
     reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
